110cef259a5b7b886ea9349e823a763700b8f478
SiemensD <d.siemens71@gmail.com>
2021-08-30 16:40:34 +0300

    Добавьте файлы проекта.

---
+++ Project2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n = 10;
+	
+	vector<vector<int> > a(n, vector<int>(n));
+	
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			a[i][j] = (i+1) * (j+1);
+		}
+	}
+
+	for (int i = 0; i < n; ++i)
+	{   
+		for (int j = 0; j < n; ++j)
+		{
+			cout << a[i][j] << "\t";
+		}
+		cout << endl;
+		
+	}
+
+}
+
